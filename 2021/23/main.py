27233d1d39c9b713c5aa5da9793a5681a8fb50f4
dan-j <5727701+dan-j@users.noreply.github.com>
2021-12-24 00:56:35 +0000

    2021 day 23... who needs code anyway?

---
+++ 2021/23/main.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+
+def part1():
+    print("do part 1 by hand!")
+
+
+def part2():
+    print("do part 2 by hand!")
+
+
+if __name__ == '__main__':
+    part1()
+    part2()
